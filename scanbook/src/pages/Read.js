244b14165697a891a9383c2be7993e53ff66c6ad
lily516liu <lily516liu@gmail.com>
2021-11-05 09:03:10 +0800

    加入Roter

---
+++ scanbook/src/pages/Read.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Read() {
+  return <div></div>;
+}
+
+export default Read;
---
+++ scanbook/src/pages/Twice.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Twice() {
+  return <div></div>;
+}
+
+export default Twice;
